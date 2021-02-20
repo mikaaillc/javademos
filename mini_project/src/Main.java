49395a13b019e6f16bca75a13f5adf05c04aa523
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 18:51:39 +0300

    20.02.2021

--- mini_project/src/Main.java
+++ mini_project/src/Main.java
@@ -35,7 +35,7 @@ public class Main {
         }*/
         //////////////////////////////////////////////////////////////////////
         //6 -->1 , 2 ,3
-        int number = 5;
+        /*int number = 5;
         int total=0;
         for (int i = 1; i < number; i++) {
             if (number % i == 0) {
@@ -47,8 +47,43 @@ public class Main {
                     System.out.println("Bu sayı mükemmel değildir");
                 }
             }
+        }*/
+        ///////////////////////////////////////////////////////////////////////
+        ///220-284
+        /*int sayi = 220;
+        int sayi2 = 284;
+        int toplam1 = 0;
+        int toplam2 = 0;
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam1 = toplam1 + i;
+            }
         }
-
+        for (int i = 1; i < sayi2; i++) {
+            if (sayi2 % i == 0) {
+                toplam2 = toplam2 + i;
+            }}
+            if (sayi == toplam2 && sayi2== toplam1) {
+                System.out.println("bu iki sayı ardakaştır");
+            }else{
+                System.out.println("bu iki sayı arkdadaş değildir");
+            }*/
+            /////////////////////////////////////////////////////////////////////
+            int[] sayilar = new int[]{1,2,3,5,6,7,4,1};
+            int aranacak =50 ;
+            boolean varMi=false;
+        for (int sayi:sayilar) {
+            if (sayi== aranacak){
+                varMi=true;
+                break;
+            }
+        }
+        if (varMi) {
+            System.out.println("Sayi dizide mevcut");
+        }
+        else{
+            System.out.println("Sayı mevcut değil");
+        }
 
     }
 }
