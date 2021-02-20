8d47c903aea1c6ad3fd0b622a21da0434e3ee11c
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 14:33:17 +0300

    20.02.2021

--- arraysdemo/src/Main.java
+++ arraysdemo/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String[] ogrenciler = new String[4]; // dizi oluşturuldu
+	    ogrenciler[0]="Ali";
+	    ogrenciler[1]="Ahmet";
+	    ogrenciler[2]="Salih";
+	    ogrenciler[3]="veli";
+
+        for (int i = 0; i<  ogrenciler.length ; i++) {
+            System.out.println(ogrenciler[i]);
+        }
     }
 }
