d496ceffd111f90e1a2bd8ca3003b22733dd65e0
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 23:57:29 +0300

    21.02.2021

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,9 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Mustei eklendi.");
+    } public void Remove(){
+        System.out.println("Mustei kaldırıldı.");
+    } public void Update(){
+        System.out.println("Mustei guncellendi.");
+    }
+}
