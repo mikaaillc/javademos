7f88aced59cbe60c5b9d3c9b4e1085929ef4a6f8
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 14:48:52 +0300

    20.02.2021

--- arraysdemo/src/Main.java
+++ arraysdemo/src/Main.java
@@ -1,17 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-	    String[] ogrenciler = new String[4]; // dizi oluşturuldu
-	    ogrenciler[0]="Ali";
-	    ogrenciler[1]="Ahmet";
-	    ogrenciler[2]="Salih";
-	    ogrenciler[3]="veli";
+//	    String[] ogrenciler = new String[4]; // dizi oluşturuldu
+//	    ogrenciler[0]="Ali";
+//	    ogrenciler[1]="Ahmet";
+//	    ogrenciler[2]="Salih";
+//	    ogrenciler[3]="veli";
+//
+//        for (int i = 0; i<  ogrenciler.length ; i++) {
+//            System.out.println(ogrenciler[i]);
+//        }
+//        for (String ogrenci:ogrenciler) {
+//            System.out.println((ogrenci));
+//        }
+        double[] myList={1.2,1.3,1.4,5.4,5.6};
 
-        for (int i = 0; i<  ogrenciler.length ; i++) {
-            System.out.println(ogrenciler[i]);
+        for (double number:myList) {
+            System.out.println(number);
+
         }
-        for (String ogrenci:ogrenciler) {
-            System.out.println((ogrenci));
-        }
+
     }
 }
