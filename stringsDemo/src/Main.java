9a020219765ed9e896669b9dd8bde5db93231dba
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 15:14:01 +0300

    20.02.21

--- stringsDemo/src/Main.java
+++ stringsDemo/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	String mesaj ="BUgğn Hava Güzel";
+	String mesaj ="Bugün Hava Güzel";
+
 	System.out.println(mesaj);
+	System.out.println("Eleman sayısı "+mesaj.length());
+	System.out.println("5.eleman "+mesaj.charAt(4));// string ifadenin 4. elemanı
     }
 }
