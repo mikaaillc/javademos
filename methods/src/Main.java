cd622b256c074ca4ec8cbc6f4a2e394835410dcc
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-20 19:42:22 +0300

    20.02.21

--- methods/src/Main.java
+++ methods/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println(yeni);
         int sayi  = topla(15,7);
         System.out.println(sayi);
+        int toplam = topla2(2,3,6,4);
+        System.out.println(toplam);
     }
     public static void ekle(){
         System.out.println("Eklendi");
@@ -20,6 +22,15 @@ public class Main {
     public static  int topla(int sayi1, int sayi2){// bu fonsiyon in şeklinde bir değer döndürür
         return sayi1+sayi2;
     }
+
+    public static int topla2(int... sayilar){ // veriables arguments args
+        int toplam=0;
+        for (int sayi:sayilar) {
+            toplam+=sayi;
+        }
+        return toplam;
+    }
+
     public static String sehir(){
         return "Ankara";
     }
