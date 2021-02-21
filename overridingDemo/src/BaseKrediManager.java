9abf842821f6ff189d7a10a0bc717f9d0a10fbee
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-21 14:31:13 +0300

    21.02.2021

---
+++ overridingDemo/src/BaseKrediManager.java
@@ -0,0 +1,6 @@
+public class BaseKrediManager {
+    public final double hesapla(double tutar){ // override edilmemesi için final kelimesi kullnılır
+        return tutar*1.18;
+    }
+}
+//overridable
--- overridingDemo/src/Main.java
+++ overridingDemo/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    BaseKrediManager[] krediManagers = new BaseKrediManager[]{new OgretmenKrediManager(),new TarimKrediManager(), new OgrenciKrediManager()};
+
+	    for(BaseKrediManager krediManager:krediManagers){
+            System.out.println(krediManager.hesapla(1000));
+        }
+
     }
 }
