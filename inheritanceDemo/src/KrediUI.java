e567fba9d831201031829a6bd46453aff0b2aa04
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-21 13:39:46 +0300

    21.02.2021

---
+++ inheritanceDemo/src/KrediUI.java
@@ -0,0 +1,5 @@
+public class KrediUI {
+    public void KrediHesapla(BaseKrediManager baseKrediManager){//base kredi manager diğer managerların referansını tuatbilir
+        baseKrediManager.Hesapla();
+    }
+}
