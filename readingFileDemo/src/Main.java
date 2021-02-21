97193085f56b09be22165bcd63db9ee4b09cfba5
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-22 01:38:34 +0300

    22.02.2021

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -1,6 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BufferedReader reader =null;
+        try {
+            reader=new BufferedReader(new FileReader("C:\\Users\\asus\\Desktop\\javademos\\readingFileDemo\\src\\sayilar" +
+                    ".txt"));
+        } catch (FileNotFoundException e) {// dosya bulamazsam hatasını otomatik ekledik
+            e.printStackTrace();
+        }finally {
+            try {// bu try catch bloğunu eğer dosya bulunmaz ise kapatılacak dosyada olmayacağı koyuyoruz
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
