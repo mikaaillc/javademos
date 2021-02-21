640444fdc41d008d9396dba5fe708709e8872f25
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-22 01:48:15 +0300

    22.02.2021

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -5,14 +5,24 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {// try catc içine yazmak zorunda
         BufferedReader reader =null;
+        int total =0;
         try {
             reader=new BufferedReader(new FileReader("C:\\Users\\asus\\Desktop\\javademos\\readingFileDemo\\src\\sayilar" +
                     ".txt"));
+            String  line =null;
+            while ((line =reader.readLine())!=null) {
+                total+=Integer.valueOf(line);// line string geleceğinden integera çevirir
+            }
+            System.out.println("Toplam:"+total);
         } catch (FileNotFoundException e) {// dosya bulamazsam hatasını otomatik ekledik
             e.printStackTrace();
-        }finally {
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             try {// bu try catch bloğunu eğer dosya bulunmaz ise kapatılacak dosyada olmayacağı koyuyoruz
                 reader.close();
             } catch (IOException e) {
