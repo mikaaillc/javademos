82fc7df71a1f92f8a337af70ee9f249688ef8d7e
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-22 21:29:14 +0300

    22.02.2021

---
+++ throwDemo/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        AccountManager accountManager =new AccountManager();
+        System.out.println("hesap= "+accountManager.getBalance());
+        accountManager.deposit(100);
+        System.out.println("hesap= "+accountManager.getBalance());
+        try {
+            accountManager.withdraw(90);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        System.out.println("hesap= "+accountManager.getBalance());
+        try {
+            accountManager.withdraw(20);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());;
+        }
+        System.out.println("hesap= "+accountManager.getBalance());
+    }
+}
