bf5df5f7ed55c6178e7c9f3ec2a79ddf60e432da
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-23 11:19:50 +0300

    23.02.2021

---
+++ workingWithFiles/src/Main.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String _file="C:\\Users\\asus\\Desktop\\javademos\\files\\students.txt";
+            //createFile(_file);
+            //getFileInfo(_file);
+            readFile(_file);
+
+
+    }
+    public static void createFile(){
+        File file =new File("C:\\Users\\asus\\Desktop\\javademos\\files\\students.txt");
+        try {
+            if(file.createNewFile()){// burada true yada false değer döner
+                System.out.println("Dosya oluşturuldu.");}
+            else{
+                System.out.println("Dosya mevcut");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void getFileInfo(String _file){
+        File file =new File(_file);
+        if(file.exists()){//BU komut dosya var ise true döndürür
+            System.out.println("Dosya adı:"+file.getName());//Burada dosyanın ismini okuruz
+            System.out.println("Dosya Yolu:"+file.getAbsolutePath());// getAbsolutePath ile konum
+            // okunabilir
+            System.out.println("Dosya yazılabiliir Mi?:"+file.canWrite());
+            System.out.println("Dosya okunabilir Mi?:"+file.canRead());
+            System.out.println("Dosya boyutu:(byte)"+file.length());
+
+        }
+    }
+    public static void readFile(String _file){
+        File file =new File(_file);
+        try {
+            Scanner reader =new Scanner(file);
+            while (reader.hasNextLine()){//okuyabileceği satır olduğu sürece devam et demek oluyor
+                String line = reader.nextLine();//satır varsa oku
+                System.out.println(line);
+            }
+            reader.close();// dosyanın kapatılması gerekir
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
