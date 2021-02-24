e5ce64ef7a3c3087cd0641f3576a4d74c63ca998
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-24 12:39:32 +0300

    24.02.2021

---
+++ jdbsIntro/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //jdps kütüphaneleri ile data base e bağlanılır
+        //java-->jdbs--> oracle/mysql/vs Driverlar --> Database
+        
+    }
+}
