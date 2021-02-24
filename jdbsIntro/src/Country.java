81f056f145a78f379f1cd51e45723cdcc1ac4c79
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-24 16:17:42 +0300

    24.02.2021

---
+++ jdbsIntro/src/Country.java
@@ -0,0 +1,11 @@
+public class Country {
+    private String code, name, continent, region;
+
+    public Country(String code,String name,String continent,String region){
+    this.code=code;
+    this.continent=continent;
+    this.name=name;
+    this.region=region;
+    }
+
+}
--- jdbsIntro/src/Main.java
+++ jdbsIntro/src/Main.java
@@ -1,29 +1,71 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
-    static String userName="";
-    static String password="2323Mk--";
-    static String dbUrl="jdbc:mysql://localhost:3306/world";
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws SQLException {
         //jdps kütüphaneleri ile data base e bağlanılır
         //java-->jdbs--> oracle/mysql/vs Driverlar --> Database
-        Connection connection =null;
-        try {
-            connection = DriverManager.getConnection(dbUrl,userName,password);
-            System.out.println("Bağlantı oluştu");
-        } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
-        }finally {
-            try {
-                connection.close();
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
 
-            } catch (SQLException exception) {
-                exception.printStackTrace();
-            }
+        PreparedStatement statement = null;//insert bu statment ile yapılır
+
+        ResultSet resultSet;
+
+        try {
+            connection = helper.getConnection();
+            String sql="insert into world.city (name ,countryCode,district ,population) values(?," +
+                    "?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"Yozgat");
+            statement.setString(2,"TUR");
+            statement.setString(3,"Yozgat");
+            statement.setInt(4,200000);
+
+            statement.executeUpdate();// update işlemleri statement hazılandıktan
+            // sonra bu
+            // şekilde yapılır
+            System.out.println("Kayıt Eklendi:");
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+            System.out.println(exception.getMessage());
+        } finally {
+            statement.close();
+            connection.close();
         }
 
+
+    }
+
+    public static void selectDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+        Statement statement = null;
+        ResultSet resultSet;
+
+        try {
+            connection = helper.getConnection();
+            statement = connection.createStatement();// bu bağlantı için creat etmemiz gerekir
+            resultSet = statement.executeQuery("select code,Name,Continent,region from world.country");
+            ArrayList<Country> countries = new ArrayList<>();
+
+            while (resultSet.next()) {
+                countries.add(new Country(
+                        resultSet.getString("code"),
+                        resultSet.getString("name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("region")));
+            }
+            System.out.println(countries.size());
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+            System.out.println(exception.getMessage());
+        } finally {
+            statement.close();
+            connection.close();
         }
     }
+}
 
