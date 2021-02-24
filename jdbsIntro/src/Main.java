ef0d95cd6bffe4ee42cba4c6af705e440498508c
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-24 13:48:19 +0300

    24.02.2021

--- jdbsIntro/src/Main.java
+++ jdbsIntro/src/Main.java
@@ -1,8 +1,30 @@
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    public static void main(String[] args) {
+public class Main {
+    static String userName="";
+    static String password="2323Mk--";
+    static String dbUrl="jdbc:mysql://localhost:3306/world";
+    public static void main(String[] args){
         //jdps kütüphaneleri ile data base e bağlanılır
         //java-->jdbs--> oracle/mysql/vs Driverlar --> Database
-        
+        Connection connection =null;
+
+        try {
+            connection = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("Bağlantı oluştu");
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }finally {
+            try {
+                connection.close();
+
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        }
     }
-}
+
