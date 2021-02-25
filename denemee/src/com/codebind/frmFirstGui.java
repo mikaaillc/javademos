3c9ef7e32d8695cc79ed1947ed4fe289bb33ab22
mikaaillc <54719338+mikaaillc@users.noreply.github.com>
2021-02-25 17:08:46 +0300

    25.02.2021

---
+++ denemee/src/com/codebind/frmFirstGui.java
@@ -0,0 +1,19 @@
+package com.codebind;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class frmFirstGui {
+    private JButton button1;
+    private JPanel panel1;
+
+    public frmFirstGui() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
